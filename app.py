c12fdac190804f09a86bf8fed59a4494bc056a37
Shubham <shubhamlad33@gmail.com>
2022-07-01 23:43:08 +0530

    Chaning the app file output

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app=Flask(__name__)
 
 @app.route("/",methods=["GET", "POST"])
 def index():
-    return "Welcome to Machine Learning Project"
+    return "CI/CD Pipeline Created"
 
 if __name__=="__main__":
     app.run(debug=True)
